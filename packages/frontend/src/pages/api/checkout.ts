7921505ef1d615beb572e9b520ad588a1987fe74
mottox2 <y.mottox2@gmail.com>
2020-11-01 03:54:55 +0900

    Implement simple payment

---
+++ packages/frontend/src/pages/api/checkout.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2020-08-27',
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const params: Stripe.Checkout.SessionCreateParams = {
+        submit_type: 'pay',
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            name: 'simple payment',
+            amount: 1000,
+            currency: 'jpy',
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/`,
+      }
+      const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(
+        params
+      )
+
+      res.status(200).json(checkoutSession)
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message })
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
---
+++ packages/frontend/src/pages/simple-payment.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { getStripe } from "../libs/stripe";
+
+const SimplePayment = () => {
+  const handleClick = async () => {
+    const res = await fetch("/api/checkout", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({}),
+    });
+    const checkoutSession = await res.json();
+    console.log(checkoutSession);
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSession.message);
+      return;
+    }
+
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+    console.warn(error.message);
+  };
+  return (
+    <div>
+      <p>Payment Page</p>
+      <button onClick={handleClick}>Checkout</button>
+    </div>
+  );
+};
+
+export default SimplePayment;
