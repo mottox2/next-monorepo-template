da91c02df2794e362e7d6e24d0f6d131f0316b8e
mottox2 <y.mottox2@gmail.com>
2020-11-01 00:35:59 +0900

    Add functions triggered by authentication

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+admin.initializeApp()
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const createUser = functions.auth.user().onCreate(user => {
+  const { uid, email, displayName} = user
+  return admin.firestore().collection('users').doc(uid).create({
+    uid, email, displayName
+  })
+})
+
+export const deleteUser = functions.auth.user().onDelete(user => {
+  return admin.firestore().collection('users').doc(user.uid).delete()
+})
